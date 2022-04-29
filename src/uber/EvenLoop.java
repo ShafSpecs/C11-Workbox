6128ec9be86efeda248e42969510d24c3e3a926f
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-04-29 23:57:40 +0100

    Part one handled.

---
+++ src/uber/EvenLoop.java
@@ -0,0 +1,10 @@
+package uber;
+
+public class EvenLoop {
+    public static void main (String[] args) {
+        for(int i = 0; i < 21;) {
+            System.out.print(" " + i + " ");
+            i += 2;
+        }
+    }
+}
