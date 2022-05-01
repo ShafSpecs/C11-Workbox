224e9d19d7f0cb9b054b2c3dd5fca05fcc4f49ca
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-05-01 12:58:25 +0100

    Max and Min number detector created

---
+++ src/uber/LargeSmallNum.java
@@ -0,0 +1,39 @@
+package uber;
+
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LargeSmallNum {
+    public static void main (String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Enter your set of numbers: ");
+
+        int originalArr[] = {};
+
+        int n = 0;
+
+        for (int i = 0; i < 10;) {
+            int newNum = userInput.nextInt();
+            if (newNum >= 0) {
+                originalArr = addArrayElements(n, originalArr, newNum);
+                n += 1;
+            } else {
+                Arrays.sort(originalArr);
+                System.out.println("The minimum number is: " + originalArr[0]);
+                System.out.println("The maximum number is: " + originalArr[n - 1]);
+                break;
+            }
+        }
+    }
+
+    public static int[] addArrayElements(int n, int array[], int newNum) {
+        int[] newArr = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            newArr[i] = array[i];
+        }
+        newArr[n] = newNum;
+        return newArr;
+    }
+}
