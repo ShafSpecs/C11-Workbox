3c1b0b326dda9099ebf084a1e9c7c97734644dd2
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-05-13 13:11:18 +0100

    Removed some unwanted lines

--- src/uber/Questionnaire.java
+++ src/uber/Questionnaire.java
@@ -109,26 +109,18 @@ public class Questionnaire {
         // Base Case: Given expression is empty
         if (exp.length() == 0) return -1;
 
-        // The first character must be
-        // an operand, find its value
         int res = value(exp.charAt(0));
 
-        // Traverse the remaining characters in pairs
         for (int i = 1; i < exp.length(); i += 2) {
-            // The next character must be an operator, and
-            // next to next an operand
             char opr = exp.charAt(i), opd = exp.charAt(i + 1);
 
-            // If next to next character is not an operand
             if (isOperand(opd) == false) return -1;
 
-            // Update result according to the operator
             if (opr == '+') res += value(opd);
             else if (opr == '-') res -= value(opd);
             else if (opr == '*') res *= value(opd);
             else if (opr == '/') res /= value(opd);
 
-                // If not a valid operator
             else return -1;
         }
         return res;
