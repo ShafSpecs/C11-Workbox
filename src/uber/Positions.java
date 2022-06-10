98e1a47182aaf51d3ac16d60f5433dada223b73d
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-06-10 16:11:39 +0100

    Tortoise vs Hare incarnation.

---
+++ src/uber/Positions.java
@@ -0,0 +1,20 @@
+package uber;
+
+enum Player {
+    SNAIL,
+    RABBIT;
+}
+
+enum SnailMoves {
+    FAST_PLOD,
+    SLOW_PLOD,
+    SLIP;
+}
+
+enum RabbitMoves {
+    SLEEP,
+    BIG_HOP,
+    SMALL_HOP,
+    BIG_SLIP,
+    SMALL_SLIP;
+}
