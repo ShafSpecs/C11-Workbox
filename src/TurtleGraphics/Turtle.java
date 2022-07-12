65a65c6a2f07e0ee0e696b51cbac15dd65a41832
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-07-12 23:37:56 +0100

    Updated and finished (*cough*) Turtle

--- src/TurtleGraphics/Turtle.java
+++ src/TurtleGraphics/Turtle.java
@@ -55,9 +55,10 @@ public class Turtle {
 
     public void move(int numberOfSteps, Sketchpad draw) {
         if(!penIsUp()) writeOn(numberOfSteps, draw);
-        move(numberOfSteps);
+        move(numberOfSteps, draw, true);
     }
 
+    @SuppressWarnings("DuplicatedCode")
     private void writeOn(int numberOfSteps, Sketchpad draw) {
         int[][] floor = draw.getFloor();
 
@@ -66,25 +67,76 @@ public class Turtle {
 
         switch (currentDirection) {
             case EAST -> {
-                for (int i = currentColumn; i < currentColumn + numberOfSteps; i++) {
-                    floor[currentRow][i] = 1;
+                for (int i = currentColumn; i <= currentColumn + numberOfSteps; i++) {
+                    if(currentColumn + numberOfSteps < floor.length) {
+                        floor[currentRow][i] = 1;
+                    } else {
+                        writeOn((floor.length - 1) - currentPosition.getX(), draw);
+                    }
+                }
+            }
+
+            case WEST -> {
+                for (int i = currentColumn; i >= currentColumn - numberOfSteps; i--) {
+                    if(currentColumn + numberOfSteps < floor.length) {
+                        floor[currentRow][i] = 1;
+                    } else {
+                        writeOn((floor.length - 1) - currentPosition.getX(), draw);
+                    }
+                }
+            }
+
+            case NORTH -> {
+                for (int i = currentRow; i >= currentRow - numberOfSteps; i--) {
+                    if(currentColumn + numberOfSteps < floor.length) {
+                        floor[i][currentColumn] = 1;
+                    } else {
+                        writeOn((floor.length - 1) - currentPosition.getY(), draw);
+                    }
+                }
+            }
+
+            case SOUTH -> {
+                for (int i = currentRow; i <= currentRow + numberOfSteps; i++) {
+                    if(currentColumn + numberOfSteps < floor.length) {
+                        floor[i][currentColumn] = 1;
+                    } else {
+                        writeOn((floor.length - 1) - currentPosition.getY(), draw);
+                    }
                 }
             }
         }
     }
 
-    private void move(int numberOfSteps) {
+    private void move(int numberOfSteps, Sketchpad draw, boolean trueMoveMethod) {
         int column = currentPosition.getX();
         int row = currentPosition.getY();
+        int[][] floor = draw.getFloor();
 
         switch (currentDirection){
-            case EAST -> currentPosition.setX(column + numberOfSteps);
+            case EAST -> {
+                if((currentPosition.getX() + numberOfSteps) < floor.length) {
+                    currentPosition.setX(column + numberOfSteps);
+                }
+            }
 
-            case WEST -> currentPosition.setX(column - numberOfSteps);
+            case WEST -> {
+                if(numberOfSteps <= currentPosition.getX()) {
+                    currentPosition.setX(column - numberOfSteps);
+                }
+            }
 
-            case NORTH -> currentPosition.setY(row + numberOfSteps);
+            case NORTH -> {
+                if(numberOfSteps <= currentPosition.getY()) {
+                    currentPosition.setY(row - numberOfSteps);
+                }
+            }
 
-            case SOUTH -> currentPosition.setY(row - numberOfSteps);
+            case SOUTH -> {
+                if((currentPosition.getY() + numberOfSteps) < floor.length) {
+                    currentPosition.setY(row + numberOfSteps);
+                }
+            }
         }
     }
 
